1c34ee8e1c5bd9374c7d95c4b53cd610a6607095
Chae Min Lim <coals722@naver.com>
2024-09-09 17:42:36 +0900

    setting: initial

---
+++ src/test/java/backend/keumbangresource/KeumbangResourceApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.keumbangresource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeumbangResourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
